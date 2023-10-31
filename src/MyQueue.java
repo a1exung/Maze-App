3ee7d510b657dc2b9956e1ec1dcd42aed8818606
Anay Apte <109878765+AnayApte@users.noreply.github.com>
2023-10-31 14:19:36 -0500

    changes

---
+++ src/MyQueue.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class MyQueue implements QueueADT
+{
+    private Node first;
+
+    public MyQueue()
+    {
+        this.first = null;
+    }
+
+    public void enqueue(Object item)
+    {
+        Node current = first;
+        while(current.next != null)
+        {
+            current = current.next;
+        }
+        Node newItem = new Node();
+        newItem.data = item;
+        current.next = newItem;
+    }
+
+    public Object dequeue() throws NoSuchElementException
+    {
+        if(first != null)
+        {
+            Node returned = first;
+            first.next = first;
+            return returned.data;
+        }
+        else
+            throw new NoSuchElementException();
+    }
+
+    public Object front() throws NoSuchElementException
+    {
+        if(first != null)
+            return first.data;
+        else
+            throw new NoSuchElementException();
+    }
+
+    public int size()
+    {
+        Node current = first;
+        int count = 1;
+        while(current.next != null)
+        {
+            current = current.next;
+            count++;
+        }
+        return count;
+    }
+
+    public boolean isEmpty()
+    {
+        return first == null;
+    }
+
+    public void clear()
+    {
+        first = null;
+    }
+
+    static class Node
+    {
+        public Object data;
+        public Node next;
+    }
+}
