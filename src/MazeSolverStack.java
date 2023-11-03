49a1680c9377f74337c0f7b32ed3fe45ff87b1fd
a1exung <ajung@stu.naperville203.org>
2023-11-03 14:14:23 -0500

    just copy and pasted stacksolver

--- src/MazeSolverStack.java
+++ src/MazeSolverStack.java
@@ -1,3 +1,31 @@
+import java.util.*;
+
 public class MazeSolverStack extends MazeSolver {
     
+    static MyStack worklist;
+
+    public MazeSolverStack(Maze maze){
+        super(maze);
+
+        makeEmpty();
+        add(maze.getStart());
+        maze.getStart().setPrevious(null);
+    }
+
+    public void makeEmpty(){
+        worklist = new MyStack();
+    }
+    
+    public boolean isEmpty(){
+        return worklist.isEmpty();
+    }
+
+    public void add(Square sq){
+        worklist.push(sq);
+    }
+
+    public Square next(){
+        return (Square) worklist.pop();
+    }
+
 }
