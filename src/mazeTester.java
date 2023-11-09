422a4c8ccb21a98ecf6eef851a5adc7a905afa8a
a1exung <110874630+a1exung@users.noreply.github.com>
2023-11-09 10:22:40 -0600

    comments on mazeTester

--- src/mazeTester.java
+++ src/mazeTester.java
@@ -6,6 +6,7 @@ public class mazeTester {
 
     public static void main(String[] args){
 
+        //test the loading of the maze
         try{
             System.out.println(loadMazeError());
             System.out.println(loadMazeTest());
@@ -13,33 +14,42 @@ public class mazeTester {
 
         System.out.println();
 
+        //test the getNeighbors for a corner square
         ArrayList<Square> neighborsTest = new ArrayList<>();
         neighborsTest = getNeighborsCorners();
 
+        //print to check
         for (Square s : neighborsTest){
             System.out.print(s);
         }
 
+        //test the getNeighbors for an edge square
         System.out.println();
         neighborsTest = getNeighborsEdge();
 
+        //print to check
         for (Square s : neighborsTest){
             System.out.print(s);
         }
 
+        //test the getNeighbors for a center square
         System.out.println();
         neighborsTest = getNeighborsMiddle();
 
+        //print to check
         for (Square s : neighborsTest){
             System.out.print(s);
         }
 
+        //test print the maze
         System.out.println();
         System.out.println(toStringTest());
 
+        //check to ensure getStart returns the correct square
         System.out.println();
         System.out.println(getStartTest());
 
+        //check to ensure getFinish returns the correct square
         System.out.println();
         System.out.println(getFinishTest());
 
