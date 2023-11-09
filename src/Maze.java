0619be1f91005e3d3af6e2f62a9fb0bd3b9cc5e8
a1exung <110874630+a1exung@users.noreply.github.com>
2023-11-09 10:20:37 -0600

    comments on Maze

--- src/Maze.java
+++ src/Maze.java
@@ -3,24 +3,32 @@ import java.io.*;
 
 public class Maze {
 
+    //declare instance variables
     private int numRows, numCols;
     public Square[][] maze;
-    
+
+    //default the Maze to have zero columns and rows
     public Maze()
     {
         numRows = 0;
         numCols = 0;
     }
 
+    /**
+     * @param String file name
+     * @return boolean success of maze load
+     */
     public boolean loadMaze(String fname)
     {
-        // Account for Errors, otherwise this works
-       
-       try{
+    
+        //try catch to find any file loading errors   
+       try{ 
             Scanner sc = new Scanner(new File(fname));
             numRows = sc.nextInt();
             numCols = sc.nextInt();
             maze = new Square[numRows][numCols];
+
+           //nested for loop to populate the Maze array
             for(int i = 0; i < numRows; i++)
             {
                 for(int j = 0; j < numCols; j++)
@@ -30,37 +38,45 @@ public class Maze {
             }
             return true;
         } catch (Exception e) {
+           //print error message if file is not loaded
             System.out.println("File could not be read");
             return false;
         }
     }
 
+    /**
+     * @return ArrayList each of the squares neighbors
+     */
     public ArrayList<Square> getNeighbors(Square sq)
     {
         ArrayList<Square> neighbors = new ArrayList<>();
+        //add the above
         if(sq.getRow()-1 >= 0 && maze[sq.getRow()-1][sq.getCol()] != null)
         {
             neighbors.add(maze[sq.getRow()-1][sq.getCol()]);
         }
+        //add the right
         if(sq.getCol()+1 < numCols && maze[sq.getRow()][sq.getCol()+1] != null)
         {
             neighbors.add(maze[sq.getRow()][sq.getCol()+1]);
         }
+        //add the below
         if(sq.getRow()+1 < numRows && maze[sq.getRow()+1][sq.getCol()] != null)
         {
             neighbors.add(maze[sq.getRow()+1][sq.getCol()]);
         }
+        //add the left
         if(sq.getCol()-1 >= 0 && maze[sq.getRow()][sq.getCol()-1] != null)
         {
             neighbors.add(maze[sq.getRow()][sq.getCol()-1]);
         }
-        for (Square s : neighbors){
-            System.out.println(s.getRow());
-        }
-
+        
         return neighbors;
     }
 
+    /**
+     * @return Square the maze's start square
+     */
     public Square getStart()
     {
         Square start = null;
@@ -75,6 +91,9 @@ public class Maze {
         return start;
     }
 
+    /**
+     * @return Square the maze's exit square
+     */
     public Square getFinish()
     {
         Square end = null;
@@ -89,6 +108,9 @@ public class Maze {
         return end;
     }
 
+    /**
+     * reset all Squares in the maze
+     */
     public void reset()
     {
         for (Square[] mazes : maze){
@@ -98,6 +120,9 @@ public class Maze {
         }
     }
 
+    /**
+     * print out each square in the maze
+     */
     public String toString()
     {
         StringBuilder sb = new StringBuilder();
