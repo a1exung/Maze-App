fe2c10914a794e45be9725dd6f7059f8bd6e5ae0
Anay Apte <109878765+AnayApte@users.noreply.github.com>
2023-11-09 20:07:51 -0600

    Comments

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -4,11 +4,16 @@ public class MyQueue implements QueueADT
 {
     private Node first;
 
+    // Implements a Queue as a series of nodes. The default sets the first node to null.
     public MyQueue()
     {
         this.first = null;
     }
 
+    /**
+     * Add an item to the queue
+     * @param Object item that is added to the queue.
+     */
     public void enqueue(Object item)
     {
         Node newItem = new Node();
@@ -29,6 +34,11 @@ public class MyQueue implements QueueADT
         }
     }
 
+    /**
+     * Remove the front item from the queue
+     * @return the front item in the queue
+     * @throws NoSuchElementException if the queue is empty
+     */
     public Object dequeue() throws NoSuchElementException
     {
         if(first != null)
@@ -41,6 +51,11 @@ public class MyQueue implements QueueADT
             throw new NoSuchElementException();
     }
 
+    /**
+     * Return the front item in the queue without removing it
+     * @return the front item in the queue
+     * @throws NoSuchElementException if the queue is empty
+     */
     public Object front() throws NoSuchElementException
     {
         if(first != null)
@@ -49,6 +64,10 @@ public class MyQueue implements QueueADT
             throw new NoSuchElementException();
     }
 
+    /**
+     * Find how many items are in the queue
+     * @return the number of items in the queue
+     */
     public int size()
     {
         Node current = first;
@@ -61,16 +80,28 @@ public class MyQueue implements QueueADT
         return count;
     }
 
+    /**
+     * Determine if the queue is empty
+     * @return true if the size is 0, false otherwise
+     */
     public boolean isEmpty()
     {
         return first == null;
     }
 
+    /**
+     * Clear out the data structure
+     */
     public void clear()
     {
         first = null;
     }
 
+    /**
+     * Checks if a node in a series of nodes contains an object as its data
+     * @param the item that is being compares
+     * @return true if the queue contains the item, false if not.
+     */
     public boolean contains(Object o){
         if (first == null) return false;
         else {
@@ -83,6 +114,7 @@ public class MyQueue implements QueueADT
         return false;
     }
 
+    // Nested Node Class
     static class Node
     {
         public Object data;
