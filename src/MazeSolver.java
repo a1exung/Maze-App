f03391740d1ffac70edf3312ddcea61048a7a146
Anay Apte <109878765+AnayApte@users.noreply.github.com>
2023-11-09 20:25:24 -0600

    comments

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,16 +1,22 @@
 import java.util.*;
 
 public abstract class MazeSolver {
-    
+
+    // Creates a maze variable, a path variable as a general worklist, and booleans for whether the worklist is finished or if the element is the last.
+    // The four variables are protected so they can be accessed by the MazeSolverQueue and MazeSolverStack classes.
     protected static Maze maze;
     protected static Stack<Square> path;
     protected boolean finished = false, last = false;
-    
+
+    // Initializes the maze and the path.
     public MazeSolver(Maze maze){
         this.maze = maze;
         path = new Stack<>();
     }
 
+    /*
+    * ALL FOUR METHODS ARE ABSTRACT SO THEY CAN BE IMPLEMENTED IN MAZESOLVERQUEUE AND MAZESOLVERSTACK.
+    */
     abstract void makeEmpty();
     
     abstract boolean isEmpty();
@@ -19,32 +25,38 @@ public abstract class MazeSolver {
 
     abstract Square next();
 
+    // Checks if the algorithm has solved the maze
+    // This method is used to determine if there is an exit path or not.
     public boolean isSolved(){
         return finished;
     }
 
+    // Gets the path to the end of the maze.
     public String getPath(){
+        // If the maze worklist isn't empty and if the next coordinate isn't the exit, then there is no solution path.
         if (isEmpty() && !last){
             return "There is no solution path";
         } else {
-
+            /*
+            * Since the worklist is a stack, we must add coordinates backwards so they are popped in order. Therefore, we add the finish first.
+            * Adds more elements to the worklist as long as the previous isn't null (if it is, it's the first element.
+            * Prints out the order by popping backwards.
+            */
             Square current = maze.getFinish();
             String path = "[" + current.getRow() + ", " + current.getCol() + "]";
 
-
-            //StringBuilder sb = new StringBuilder();
             while (current.getPrevious() != null) {
                 current = current.getPrevious();
                 current.onPath();
-                //sb.append("[" + sq.getRow() + ", " + sq.getCol() + "]\n");
                 path = "[" + current.getRow() + ", " + current.getCol() + "] " + path;
             }
-            //String toReturn = new String(sb);
             return "The solution path is: " + path;
         }
     }
 
+    // Performs each individual step
     public Square step(){
+        // Calls on the isEmpty method. If worklist empty, then the puzzle is solved and no square returned.
         if (isEmpty()){
             finished = true;
             return null;
@@ -53,11 +65,14 @@ public abstract class MazeSolver {
         else {
             Square next = next();
 
+            // Checks if the next square is the exit, returns it if it is.
             if (next.getType() == 3){
                 finished = true;
                 last = true;
                 return next;
             }
+            // Adds the neighbors of each square, checks to make sure that they're either empty spaces or the exit to be added.
+            // Only elements that aren't already in the worklist are added.
             else {
                 ArrayList<Square> neighbors = maze.getNeighbors(next);
                 
@@ -81,7 +96,7 @@ public abstract class MazeSolver {
     }
 
     /**
-     * 
+     * If the puzzle isn't solved, we keep calling step.
      */
     public void solve(){
         while (!isSolved()){
