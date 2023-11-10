650ebc676c6311c808f48b9b5a27dbe6e364c5c4
Anay Apte <109878765+AnayApte@users.noreply.github.com>
2023-11-09 20:28:46 -0600

    comments

--- src/MazeSolverQueue.java
+++ src/MazeSolverQueue.java
@@ -1,26 +1,29 @@
 public class MazeSolverQueue extends MazeSolver
 {
-    static MyQueue worklist;
+    static MyQueue worklist; // Iniitializes the worklist as a Queue
 
     public MazeSolverQueue(Maze maze)
     {
-        super(maze);
+        super(maze); // Uses the parent constructor to inherit all methods
 
-        makeEmpty();
-        add(maze.getStart());
-        maze.getStart().setPrevious(null);
+        makeEmpty(); // Initially make the list empty.
+        add(maze.getStart()); // Adds the start.
+        maze.getStart().setPrevious(null); // Sets the previous of the start to null.
     }
 
+    // Makes the worklist empty.
     public void makeEmpty()
     {
-        worklist = new MyQueue();
+        worklist = new MyQueue(); 
     }
 
+    // Checks if the worklist is empty.
     public boolean isEmpty()
     {
         return worklist.isEmpty();
     }
 
+    // checks if the worklist already contains the square. If it doesn't and if the square isn't explored, add it to the worklist.
     public void add(Square sq)
     {
         boolean isPresent = false;
@@ -28,6 +31,7 @@ public class MazeSolverQueue extends MazeSolver
         if (!isPresent && !sq.isExplored()) worklist.enqueue(sq);
     }
 
+    // removes a square off the queue and returns it.
     public Square next()
     {
         return (Square) worklist.dequeue();
