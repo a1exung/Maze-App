02c92e5df6d852c355966b06263a9a5c49360caf
Anay Apte <109878765+AnayApte@users.noreply.github.com>
2023-11-09 20:30:43 -0600

    comments

--- src/MazeSolverStack.java
+++ src/MazeSolverStack.java
@@ -2,24 +2,27 @@ import java.util.*;
 
 public class MazeSolverStack extends MazeSolver {
     
-    static MyStack worklist;
+    static MyStack worklist; // Iniitializes the worklist as a stack.
 
     public MazeSolverStack(Maze maze){
-        super(maze);
+        super(maze); // Uses the parent constructor to inherit all methods
 
-        makeEmpty();
-        add(maze.getStart());
-        maze.getStart().setPrevious(null);
+        makeEmpty(); // Initially make the list empty.
+        add(maze.getStart()); // Adds the start.
+        maze.getStart().setPrevious(null);  // Sets the previous of the start to null.
     }
 
+    // Makes the worklist empty
     public void makeEmpty(){
         worklist = new MyStack();
     }
-    
+
+    // Checks if the worklist is empty.
     public boolean isEmpty(){
         return worklist.isEmpty();
     }
 
+    // checks if the worklist already contains the square. If it doesn't and if the square isn't explored, add it to the worklist.
     public void add(Square sq){
         boolean isPresent = false;
         for (int i = 0; i < worklist.size(); i++){
@@ -32,6 +35,7 @@ public class MazeSolverStack extends MazeSolver {
         }
     }
 
+    // removes a square off the queue and returns it.
     public Square next(){
         return (Square) worklist.pop();
     }
